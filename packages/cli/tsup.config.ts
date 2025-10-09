ed0bcaa234ff987f4954947880e89429b630f457
Gleb Stepanov <gleb.stepanov@online.de>
2025-10-09 23:36:00 +0200

    fixed dts error to build CLI project

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['./src/index.ts'],
   format: ['esm'],
   outDir: 'dist',
   clean: true,
+  dts: {
+    resolve: true,
+    compilerOptions: {
+      composite: false,
+    },
+  },
 })
